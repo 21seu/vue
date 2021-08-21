b70d5ad50d362ae028ccfb11a38a7e8e2b6b2e0e
21seu <1693376841@qq.com>
2021-08-21 22:53:09 +0800

    动态路由导航栏设置

--- vue_pro_fe/src/router/index.js
+++ vue_pro_fe/src/router/index.js
@@ -1,29 +1,56 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import App from '../App'
+import Index from "../views/Index.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
+    {
+        path: '/',
+        name: '导航',
+        component: Index,
+        redirect:"/pageOne",
+        children: [
+          {
+            path: '/pageOne',
+            name: '页面1',
+            component: () => import(/* webpackChunkName: "about" */ '../views/page/PageOne.vue')
+          },
+          {
+            path: '/pageTwo',
+            name: '页面2',
+            component: () => import(/* webpackChunkName: "about" */ '../views/page/PageTwo.vue')
+          }
+        ]
+    },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/nav',
+    name: '导航2',
+    component: Index,
+    children: [
+      {
+        path: '/pageThree',
+        name: 'pageThree',
+        component: () => import(/* webpackChunkName: "about" */ '../views/page/PageOne.vue')
+      }
+    ]
+  }/*,
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/!* webpackChunkName: "about" *!/ '../views/About.vue')
+    }*/
+
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ vue_pro_fe/src/views/page/PageTwo.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>Page Two</h3>
+</template>
+
+<script>
+    export default {
+        name: "PageTwo"
+    }
+</script>
+
+<style scoped>
+
+</style>
